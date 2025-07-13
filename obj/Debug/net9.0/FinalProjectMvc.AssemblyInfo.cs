50aa831afd4f89f81cdbd30676116bab47f7817a
SEYDA\fadimeseydademetoglu <fseydayucel@gmail.com>
2025-07-13 16:10:59 -0700

    fix: Email api key updated

--- obj/Debug/net9.0/FinalProjectMvc.AssemblyInfo.cs
+++ obj/Debug/net9.0/FinalProjectMvc.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProjectMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6993e866768e23c91d4c56e460c6344f6041a4a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60fb864069d08be02c20bb17a00f2d6c9bf5f768")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProjectMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProjectMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
